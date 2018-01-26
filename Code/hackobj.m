f9e2969b76b6284b466ae2bef23e6e4dbe1cc800
michaelmusty <michaelmusty@gmail.com>
2018-01-26 11:17:06 -0500

    basic outline

--- Code/hackobj.m
+++ Code/hackobj.m
@@ -1,6 +1,12 @@
+/*****
+BASIC IMPLEMENTATION OF HMF OBJECT
+*****/
+
 declare type HMF;
 declare attributes HMF:
   HMFPrecision, // RngIntElt : precision for the expansion
+  HMFWeights, // SeqEnum[RngIntElt] : a sequence of [HMFBaseField : QQ] integers
+  HMFLevel, // RngOrdIdl : ideal of Integers(HMFBaseField)
   HMFBaseField, // FldNum : totally real field
   HMFCoefficientRing, // Integers() or RngOrd : Integers of a number field
   HMFCoefficients; // Assoc : all ideals of HMFBaseField with norm less than or equal to HMFPrecision
@@ -11,21 +17,48 @@ intrinsic HMFInitialize() -> HMF
   return f;
 end intrinsic;
 
+intrinsic HMFCopy(f::HMF) -> HMF
+  {Copy HMF.}
+  F := f`HMFBaseField;
+  prec := f`HMFPrecision;
+  g := HMFZero(F, prec);
+  for attr in GetAttributes(Type(f)) do
+    if assigned f``attr then
+      g``attr := f``attr;
+    end if;
+  end for;
+  return g;
+end intrinsic;
+
 // TODO printing for levels "Default", "Minimal", "Maximal", "Magma"
 // TODO printing of coefficients?
 intrinsic Print(f::HMF) // this is a procedure, so no return
   {Print HMF.}
-  printf "Hilbert modular form over %o up to precision %o with coefficients in %o.\n", f`HMFBaseField, f`HMFPrecision, f`HMFCoefficientRing;
+  printf "Hilbert modular form over %o.\n", f`HMFBaseField;
+  printf "Weights: %o\n", f`HMFWeights;
+  printf "Level: %o\n", f`HMFLevel;
+  printf "Coefficient ring: %o\n", f`HMFCoefficientRing;
+  printf "Precision: %o\n", f`HMFPrecision;
 end intrinsic;
 
-intrinsic HMFZero(F::FldNum, prec::RngIntElt) -> HMF
-  {Generates the zero HMF over F with precision prec with integer coefficients.}
+// TODO
+intrinsic HMFIsAdmissibleWeight(F::FldNum, k::SeqEnum[RngIntElt]) -> BoolElt
+  {Decide if the sequence of weights k is admissible.}
+  return true; // TODO
+end intrinsic;
+
+intrinsic HMFZero(F::FldNum, N::RngOrdIdl, k::SeqEnum[RngIntElt], prec::RngIntElt) -> HMF
+  {Generates the zero HMF over F with level N, weights k, with precision prec with integer coefficients.}
   // basic assertions
   assert IsTotallyReal(F);
+  assert NumberFiedl(N) eq F;
+  assert HMFIsAdmissibleWeight(F, k);
   assert prec gt 0;
   // initialize the object
   f := HMFInitialize();
   f`HMFBaseField := F;
+  f`HMFLevel := N;
+  f`HMFWeights := k;
   f`HMFPrecision := prec;
   f`HMFCoefficientRing := Integers();
   // create associative array called coeffs
@@ -38,12 +71,14 @@ intrinsic HMFZero(F::FldNum, prec::RngIntElt) -> HMF
   return f;
 end intrinsic;
 
+/*
+// TODO
 intrinsic HMFZero(F::FldNum, K::FldRat, prec::RngIntElt) -> HMF
   {Generates the zero HMF over F with precision prec with integer coefficients.}
   return HMFZero(F, prec);
 end intrinsic;
 
-intrinsic HMFZero(F::FldNum, K::FldNum, prec::RngIntElt) -> HMF
+intrinsic HMFZero(F::FldNum, N::RngOrdIdl, K::FldNum, prec::RngIntElt) -> HMF
   {Generates the zero HMF over F with precision prec with coefficients in ZK.}
   // basic assertions
   assert IsTotallyReal(F);
@@ -63,54 +98,14 @@ intrinsic HMFZero(F::FldNum, K::FldNum, prec::RngIntElt) -> HMF
   f`HMFCoefficients := coeffs;
   return f;
 end intrinsic;
+*/
 
-intrinsic HMFCopy(f::HMF) -> HMF
-  {Copy HMF.}
-  F := f`HMFBaseField;
-  prec := f`HMFPrecision;
-  g := HMFZero(F, prec);
-  for attr in GetAttributes(Type(f)) do
-    if assigned f``attr then
-      g``attr := f``attr;
-    end if;
-  end for;
-  return g;
-end intrinsic;
 
-intrinsic '*'(c::RngIntElt, f::HMF) -> HMF
-  {scale f by integer c.}
-  g := HMFCopy(f); // new instance of f
-  coeffs := g`HMFCoefficients;
-  for i in Keys(coeffs) do
-    coeffs[i] := c * coeffs[i];
-  end for;
-  return g;
-end intrinsic;
 
-// TODO
-intrinsic '*'(c::RngOrdElt, f::HMF) -> HMF
-  {scale f by integral element c.}
-  ZK := f`HMFCoefficientRing;
-  assert Parent(c) eq ZK;
-  g := HMFCopy(f); // new instance of f
-  coeffs := g`HMFCoefficients;
-  for I in Keys(coeffs) do // I an ideal of ZF
-    coeffs[I] *:= ZK!(c); // multiplication in ZK, coercion just to be safe
-  end for;
-  return g;
-end intrinsic;
+// TODO Parent?
+// TODO IsCoercible?
 
 /*
-// TODO
-intrinsic '+'(f::HMF, g::HMF) -> HMF
-  {return f+g}
-end intrinsic;
-
-// TODO
-intrinsic '*'(f::HMF, g::HMF) -> HMF
-  {return f*g}
-end intrinsic;
-
 // TODO
 intrinsic 'eq'(s::HMF, t::HMF) -> BoolElt
   {}
@@ -124,7 +119,54 @@ intrinsic 'eq'(s::HMF, t::HMF) -> BoolElt
   end for;
   return isSame;
 end intrinsic;
-
-// TODO Parent?
-// TODO IsCoercible?
 */
+
+/*****
+ARITHMETIC
+*****/
+
+intrinsic '*'(c::RngIntElt, f::HMF) -> HMF
+  {scale f by integer c.}
+  g := HMFCopy(f); // new instance of f
+  coeffs := g`HMFCoefficients;
+  for i in Keys(coeffs) do
+    coeffs[i] := c * coeffs[i];
+  end for;
+  return g;
+end intrinsic;
+
+intrinsic '*'(c::RngOrdElt, f::HMF) -> HMF
+  {scale f by integral element c.}
+  ZK := f`HMFCoefficientRing;
+  assert Parent(c) eq ZK;
+  g := HMFCopy(f); // new instance of f
+  coeffs := g`HMFCoefficients;
+  for I in Keys(coeffs) do // I an ideal of ZF
+    coeffs[I] *:= ZK!(c); // multiplication in ZK, coercion just to be safe
+  end for;
+  return g;
+end intrinsic;
+
+/*
+intrinsic '+'(f::HMF, g::HMF) -> HMF
+  {return f+g}
+  Ff := f`HMFBaseField;
+  ZKf := f`HMFCoefficientRing;
+  Fg := g`HMFBaseField;
+  ZKg := g`HMFCoefficientRing;
+  assert Ff eq Fg;
+  assert ZKf eq ZKg;
+end intrinsic;
+*/
+
+/*
+// TODO
+intrinsic '*'(f::HMF, g::HMF) -> HMF
+  {return f*g}
+end intrinsic;
+*/
+
+/*****
+USER CONVENIENCE
+*****/
+// TODO Coefficients(f)
