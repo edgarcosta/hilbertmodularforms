d304d9a7be988b646112ec57ec920b68e138bb1f
Edgar Costa <edgarcosta@omicron.local>
2018-02-01 15:46:12 -0500

    eigenform example

--- usage.m
+++ usage.m
@@ -7,8 +7,8 @@ load "config.m";
 
 // basic inputs to creation functions
 F := QuadraticField(8);
-ZF := Integers(F);
-N := 12*ZF;
+ZF<w> := Integers(F);
+N := ideal<ZF | {9, 3, 3}>;
 k := [2, 2];
 K := Rationals();
 prec := 100;
@@ -20,4 +20,17 @@ g := HMFZero(F, N, k, K, 200);
 
 // arithmetic (currently only scalar multiplication and addition are implemented)
 
-// EigenEdgar
+
+M2 := HilbertCuspForms(F, N);
+S := NewSubspace(M2);
+prec := 100;
+newspaces := NewformDecomposition(S);
+newforms := [Eigenform(U) : U in newspaces];
+eigenvalues := AssociativeArray();
+primes := PrimesUpTo(prec, F);
+for pp in primes do
+    eigenvalues[pp] := HeckeEigenvalue(newforms[1],pp);
+end for;
+
+ef := EigenformToHMF(M, eigenvalues, prec);
+print ef;
