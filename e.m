e0d304dd76e9544b0f0eddb327f0f39fdded3297
michaelmusty <michaelmusty@gmail.com>
2018-03-05 14:52:37 -0500

    Eisenstein testing

--- e.m
+++ e.m
@@ -2,28 +2,33 @@ load "config.m";
 
 F<nu> := QuadraticField(5);
 ZF := Integers(F);
-// N := (-5*nu+2)*ZF;
 N := ideal<ZF|[1]>;
 prec := 100;
 M := HMFSpace(F, N, prec);
-
 X := HeckeCharacterGroup(N);
 triv := X!1;
 eta := Random(X);
 psi := Random(X);
 
-// E := EisensteinSeries(M, triv, triv, k);
 E2 := 120*EisensteinSeries(M, eta, psi, [2,2]);
 E4 := 240*EisensteinSeries(M, eta, psi, [4,4]);
 
 assert E2*E2 eq E4;
 
-E6 := 2520*EisensteinSeries(M, eta, psi, [6,6]);
 E8 := EisensteinSeries(M, eta, psi, [8,8]);
 E8 := (1/Coefficients(E8)[1])*E8;
 
 cusp_forms8 := CuspFormBasis(M, [8,8]);
 
 g := E8-(E4*E4);
-g_test := (1/Coefficients(g)[2])*g;
-assert g_test eq cusp_forms8[1];
+g_normalized := (1/Coefficients(g)[2])*g;
+assert g_normalized eq cusp_forms8[1];
+
+
+E6 := EisensteinSeries(M, eta, psi, [6,6]);
+E6 := (1/Coefficients(E6)[1])*E6;
+cusp_forms6 := CuspFormBasis(M,[6,6]);
+
+f := E6-(E2*E4);
+f_normalized := (1/Coefficients(f)[2])*f;
+assert f_normalized eq cusp_forms6[1];
