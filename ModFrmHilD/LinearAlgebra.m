60f22dc9ddc751b7260306a7c78ba8ca99c07134
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-03-06 10:03:48 -0500

    CoefficientsMatrix and LinearDepdence cleanup

--- ModFrmHilD/LinearAlgebra.m
+++ ModFrmHilD/LinearAlgebra.m
@@ -1,9 +1,20 @@
 
 //TODO add optional flag to limit the number of coefficients
+intrinsic CoefficientsMatrix(list::SeqEnum[ModFrmHilDElt]) -> AlgMatElt
+  {returns a matrix with the coefficients of each modular form in each row}
+  return Matrix( [ Coefficients(elt) : elt in list] );
+end intrinsic;
+
+//TODO add optional flag to limit the number of coefficients
 intrinsic LinearDepedence(list::SeqEnum[ModFrmHilDElt] ) -> SeqEnum[RngIntElt]
-  {finds a small non-trivial integral linear combination between components of v. If none can be found return an empty vector.}
+  {finds a small non-trivial integral linear combination between components of v. If none can be found return the empty vector.}
   M := Matrix( [ Coefficients(elt) : elt in list] );
-  return LLL(Basis(Kernel(M)));;
+  B := Basis(Kernel(M));
+  if #B ne 0 then
+    return Matrix(LLL(Basis(Kernel(M))));
+  else
+    return 0;
+  end if;
 end intrinsic;
 
 //EchelonBasis
