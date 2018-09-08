33785a04bc368f5624d03c5b33c382f4b7f67f86
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-09-08 13:32:23 -0400

    reusing code

--- ModFrmHilD/LinearAlgebra.m
+++ ModFrmHilD/LinearAlgebra.m
@@ -5,17 +5,7 @@ intrinsic CoefficientsMatrix(list::SeqEnum[ModFrmHilDElt]) -> AlgMatElt
   return Matrix( [ Coefficients(elt) : elt in list] );
 end intrinsic;
 
-//TODO add optional flag to limit the number of coefficients
-intrinsic LinearDependence(list::SeqEnum[ModFrmHilDElt] ) -> SeqEnum[RngIntElt]
-  {finds a small non-trivial integral linear combination between components of v. If none can be found return 0.}
-  M := Matrix( [ Coefficients(elt) : elt in list] );
-  B := Basis(Kernel(M));
-  if #B ne 0 then
-    return Matrix(LLL(Basis(Kernel(M))));
-  else
-    return 0;
-  end if;
-end intrinsic;
+
 
 intrinsic LinearDependence(list::SeqEnum[SeqEnum] ) -> SeqEnum[RngIntElt]
   {finds a small non-trivial integral linear combination between components of v. If none can be found return 0.}
@@ -26,6 +16,12 @@ intrinsic LinearDependence(list::SeqEnum[SeqEnum] ) -> SeqEnum[RngIntElt]
   else
     return 0;
   end if;
+end intrinsic;
+
+//TODO add optional flag to limit the number of coefficients
+intrinsic LinearDependence(list::SeqEnum[ModFrmHilDElt] ) -> SeqEnum[RngIntElt]
+  {finds a small non-trivial integral linear combination between components of v. If none can be found return 0.}
+  return LinearDependence([ Coefficients(elt) : elt in list] );
 end intrinsic;
 
 
