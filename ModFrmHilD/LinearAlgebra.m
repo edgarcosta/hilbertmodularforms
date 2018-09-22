55339e5109e9852ae3217f6ca524330867a999f9
Ben Breen <Benjamin.K.breen.gr@dartmouth.edu>
2018-09-22 00:37:03 -0400

    Canoncalring + Bug Fixes

--- ModFrmHilD/LinearAlgebra.m
+++ ModFrmHilD/LinearAlgebra.m
@@ -1,4 +1,3 @@
-
 //TODO add optional flag to limit the number of coefficients
 intrinsic CoefficientsMatrix(list::SeqEnum[ModFrmHilDElt]) -> AlgMatElt
   {returns a matrix with the coefficients of each modular form in each row}
@@ -12,9 +11,10 @@ intrinsic LinearDependence(list::SeqEnum[SeqEnum] ) -> SeqEnum[RngIntElt]
   M := Matrix( [ elt : elt in list] );
   B := Basis(Kernel(M));
   if #B ne 0 then
-    return Matrix(LLL(Basis(Kernel(M))));
+    Mat := Matrix(LLL(Basis(Kernel(M))));
+    return [Eltseq(i) : i in Rows(Mat)];
   else
-    return 0;
+    return [];
   end if;
 end intrinsic;
 
@@ -25,7 +25,5 @@ intrinsic LinearDependence(list::SeqEnum[ModFrmHilDElt] ) -> SeqEnum[RngIntElt]
   {finds a small non-trivial integral linear combination between components of v. If none can be found return 0.}
   return LinearDependence([ Coefficients(elt) : elt in list] );
 end intrinsic;
-
-
 
 //EchelonBasis
