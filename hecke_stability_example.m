ebc57b7f43d8fa87461f47f6963ed50a748cea26
ldsgsm <molnar.grant.5772@gmail.com>
2019-02-26 14:44:07 -0500

    Did a little bit with Hecke stability. This work is unstable and doesn't reallly do much yet!

--- hecke_stability_example.m
+++ hecke_stability_example.m
@@ -2,10 +2,27 @@ load "config.m";
 
 F := QuadraticField(5);
 ZF<w> := Integers(F);
-N := ideal<ZF | {1}>;
+N := ideal<ZF | {122}>;
 prec := 50;
 M := HMFSpace(F, prec);
 
-k := [1, 1];
+k := [6,6];
+//There's no particular reason to make these characters trivial--but they work.
+//eta := H ! 1;
+/* psi := H ! 1; */
+//psi := eta;
+//All eisenstein series we are using as candidates
+//E := EisensteinSeries(M, N, eta, psi, ke);
+//print E;
 H := HeckeCharacterGroup(N);
+print "Modulus H";
+//ListSignatures(GrpHecke);
+for eta in Elements(H) do
+  print Modulus(eta);
+end for;
+//A := Generators(H);
+//print #H;
+//print "Done";
+//print HeckeStability(M, N, k);
 
+//print #S;
