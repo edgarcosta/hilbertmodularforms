25dfbc93dc33287c9c42b3243eae974d9aa0cc8d
Edgar Costa <edgarc@mit.edu>
2019-04-29 14:05:26 -0400

    removing trailing white space

--- ModFrmHilD/Basis.m
+++ ModFrmHilD/Basis.m
@@ -7,24 +7,24 @@
 // Currently calls the Newforms and Eisenstein series from Creations folder
 
 
-//TODO should we sieve? 
+//TODO should we sieve?
 //BB - I removed the newform dimension. Do we use anywhere?      /* if dd eq N then newforms_dimension := #CuspSpace_dd; end if;  */
 intrinsic CuspFormBasis(Sp::ModFrmHilD) -> SeqEnum[ModFrmHilDElt]
-  {returns a basis for cuspspace of M of weight k}  
+  {returns a basis for cuspspace of M of weight k}
   N := Level(Sp);
   k := Weight(Sp);
-  Cuspbasis := []; 
-  for dd in Divisors(N) do 
-	 NewSpace_dd := &cat[GaloisOrbitDescent(f) : f in NewformsToHMF(Sp)]; // We are taking the Q orbits 
-    OldSpace_dd := &cat[Inclusion(elt,Sp) : elt in NewSpace_dd ]; 
-    Cuspbasis cat:= OldSpace_dd; 
-  end for; 
-  return Cuspbasis; 
-end intrinsic; 
+  Cuspbasis := [];
+  for dd in Divisors(N) do
+	 NewSpace_dd := &cat[GaloisOrbitDescent(f) : f in NewformsToHMF(Sp)]; // We are taking the Q orbits
+    OldSpace_dd := &cat[Inclusion(elt,Sp) : elt in NewSpace_dd ];
+    Cuspbasis cat:= OldSpace_dd;
+  end for;
+  return Cuspbasis;
+end intrinsic;
 
 
 // Not implemented for k = 1 currently
-//TODO - Test for correctness. Clean up code? 
+//TODO - Test for correctness. Clean up code?
 intrinsic EisensteinBasis(Sp::ModFrmHilD) -> SeqEnum[ModFrmHilDElt]
   {returns a basis for the complement to the cuspspace of M of weight k}
   N := Level(Sp);
@@ -35,13 +35,13 @@ intrinsic EisensteinBasis(Sp::ModFrmHilD) -> SeqEnum[ModFrmHilDElt]
   Hplus := HeckeCharacterGroup(1*ZF,[1..n]);
   HNplus := HeckeCharacterGroup(N,[1..n]);
 
-  for i in [0..#Hplus-1] do 
+  for i in [0..#Hplus-1] do
     eta := Hplus.i;
-    for j in [0..#HNplus-1] do 
+    for j in [0..#HNplus-1] do
       psi := HNplus.j;
       H_psi := Restrict(psi, Hplus);
       // This is checking the condition on pg 458
-      if k[1] mod 2 eq 0 then 
+      if k[1] mod 2 eq 0 then
         if H_psi*eta^(-1) eq Hplus!1 then
           E := EisensteinSeries(Sp, eta, psi);
           EB cat:= GaloisOrbitDescent(E);
@@ -61,15 +61,15 @@ end intrinsic;
 
 
 //BB - I removed the newform dimension.       CB, newforms_dimension := CuspFormBasis(M, N, k);
-intrinsic Basis(Sp::ModFrmHilD) -> SeqEnum[ModFrmHilDElt], RngIntElt 
-  { returns a Basis for the space } 
+intrinsic Basis(Sp::ModFrmHilD) -> SeqEnum[ModFrmHilDElt], RngIntElt
+  { returns a Basis for the space }
   // Cuspforms
   CB := CuspFormBasis(Sp);
   //Eisenstein Series
   EB := EisensteinBasis(Sp);
 
 return EB cat CB;
- end intrinsic; 
+ end intrinsic;
 
 
 /* intrinsic GaloisInvariantBasis(M::ModFrmHilD, N::RngOrdIdl, k::SeqEnum[RngIntElt]) -> SeqEnum[ModFrmHilDElt] */
@@ -83,7 +83,7 @@ return EB cat CB;
 /*   for x in InvariantGenerators do */
 /*     if #LinearDependence(InvariantBasis cat [x]) eq 0 then */
 /*       Append(~InvariantBasis, x); */
-/*     end if; */ 
+/*     end if; */
 /*   end for; */
 /*   return InvariantBasis; */
 /* end intrinsic; */
