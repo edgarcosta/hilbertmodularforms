58300f39b699a229267c4ed7719bf65d4f0452e5
Edgar Costa <edgarc@mit.edu>
2019-04-30 12:34:34 -0400

    lowering prec

--- examples/can_ring_example.m
+++ examples/can_ring_example.m
@@ -6,7 +6,7 @@ ZF<w> := Integers(F);
 N := ideal<ZF | {1}>;
 k := [2, 2];
 K := Rationals();
-prec := 50; // This gives 295 coefficients which is overkill (need only 50-100 for this graded ring). Can be safely set to 30
+prec := 30;
 M := GradedRingOfHMFs(F, prec);
 
 time g, r := ConstructGeneratorsAndRelations(M, N, 8);
