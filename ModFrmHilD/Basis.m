2fe6e932cac9cb040e083b61cac9409cc592b9d9
Angelica Babei <angelica.babei.gr@dartmouth.edu>
2019-05-21 12:28:46 -0400

    fix Galois nvariant basis

--- ModFrmHilD/Basis.m
+++ ModFrmHilD/Basis.m
@@ -66,18 +66,18 @@ intrinsic Basis(Mk::ModFrmHilD) -> SeqEnum[ModFrmHilDElt]
 end intrinsic;
 
 
-/* intrinsic GaloisInvariantBasis(M::ModFrmHilD, N::RngOrdIdl, k::SeqEnum[RngIntElt]) -> SeqEnum[ModFrmHilDElt] */
-/*   {returns a basis for the GaLois invariant subspace} */
-/*   B:=Basis(M,N,k); */
-/*   InvariantGenerators:=[]; */
-/*   for x in B do */
-/*     Append(~InvariantGenerators, 1/2*(x+Swap(x))); */
-/*   end for; */
-/*   InvariantBasis:=[]; */
-/*   for x in InvariantGenerators do */
-/*     if #LinearDependence(InvariantBasis cat [x]) eq 0 then */
-/*       Append(~InvariantBasis, x); */
-/*     end if; */
-/*   end for; */
-/*   return InvariantBasis; */
-/* end intrinsic; */
+intrinsic GaloisInvariantBasis(Mk::ModFrmHilD) -> SeqEnum[ModFrmHilDElt] 
+   {returns a basis for the Galois invariant subspace} 
+   B:=Basis(Mk); 
+   InvariantGenerators:=[]; 
+   for x in B do 
+     Append(~InvariantGenerators, 1/2*(x+Swap(x))); 
+   end for; 
+   InvariantBasis:=[]; 
+   for x in InvariantGenerators do 
+     if #LinearDependence(InvariantBasis cat [x]) eq 0 then 
+       Append(~InvariantBasis, x); 
+     end if; 
+   end for; 
+   return InvariantBasis; 
+ end intrinsic; 
