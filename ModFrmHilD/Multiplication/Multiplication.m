90edec3e07351acc12ed05b13af6b82fdf88487f
Benjamin Breen <Benjamin.k.breen.GR@dartmouth.edu>
2021-07-27 16:12:40 -0400

    merge conflicts

--- ModFrmHilD/Multiplication/Multiplication.m
+++ ModFrmHilD/Multiplication/Multiplication.m
@@ -1,18 +1,19 @@
-intrinsic GetIndexPairs(bb::RngOrdFracIdl, M::ModFrmHilDGRng)
-  {assigns M`MultiplicationTables[bb]  list (assoc array) of [nu, [[mu,mup],...] ] such 
-   that mu+mup = nu up to trace bound Precision(M).}
 
-  require bb in NarrowClassGroupReps(M): 
-      "bb must be among the fixed set of representatives of the narrow class group";
 
+intrinsic ComputeMPairs(bb::RngOrdFracIdl, M::ModFrmHilDGRng)
+  {sets M`MultiplicationTables[bb]  list (assoc array) of [nu, [[nu1,nu2],...] ] such that nu1+nu2 = nu up to trace bound Precision(M).}
+  require bb in NarrowClassGroupReps(M): "bb must be a represetative of a narrow class";
+  // Preliminaries
+  ZF := Integers(M);
   TraceBound := Precision(M);
   positive_reps := PositiveRepsByTrace(M); // indexed by ideal class and then trace
-  shintani_reps := ShintaniRepsByTrace(M); // indexed by ideal class and then trace
-  pairs := AssociativeArray(); // indexed by Shintani representatives
-  for nu in ShintaniReps(M)[bb] do
+  shintani_reps := ShintaniReps(M)[bb]; // List of Shintani reps for bb
+  pairs := AssociativeArray(); // keys = shintani_reps // For each nu, this stores lists of of [nu1,nu2].
+  for nu in shintani_reps do 
     pairs[nu] := [];
   end for;
 
+  //////// Algorithm : nu = nu1 + nu2  /////////
   // Add pairs of elements lexicographically by trace
   nus := [];
   for i := 0 to TraceBound do // loop over trace
@@ -52,15 +53,14 @@ intrinsic GetIndexPairs(bb::RngOrdFracIdl, M::ModFrmHilDGRng)
       end if;
     end for;
   end for;
-  
-  PopulateShintaniRepsIdeal(M, bb, SequenceToSet(nus));
-  M`MultiplicationTables[bb] := AssociativeArray();
-
-  for key in Keys(pairs) do
-    // and mu,mup,... totally positive (not necessarily in Shintani)
-    // assert IsShintaniReduced(key);
-    // above we populated M`ShintaniRepsIdeal[bb][nu] for any nu
-    M`MultiplicationTables[bb][M`ShintaniRepsIdeal[bb][key]] := 
-      [[M`ShintaniRepsIdeal[bb][pair[1]], M`ShintaniRepsIdeal[bb][pair[2]]] : pair in pairs[key]];
+  /////// Populating multiplication table ////////////
+  /* We have nu1 + nu2 = nu where nu1,nu2 are totally positive (not necessarily Shintani reduced). 
+  Write nui = mui*ei where ei is a tot. pos. unit and mui is Shintani reduced and then for each nu we will store a list of the [[mu1,e1],[mu2,e2]] */
+  M`MPairs[bb] := AssociativeArray(); // keys = shintani_reps, For each nu, this stores [[mu1,e1],[mu2,e2]].
+  for nu in shintani_reps do
+      M`MPairs[bb][nu] := [[ReduceShintaniMinimizeTrace(i) : i in pair] : pair in pairs[nu]];
   end for;
 end intrinsic;
+
+
+
