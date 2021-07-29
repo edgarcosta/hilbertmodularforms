b8195167321748a8c9972ad242550d3f3477e663
John Voight <jvoight@gmail.com>
2021-07-29 15:42:47 -0400

    div and power

--- ModFrmHilD/UnitChar.m
+++ ModFrmHilD/UnitChar.m
@@ -35,6 +35,26 @@ intrinsic '*'(omega::GrpCharUnitTotElt, omegap::GrpCharUnitTotElt) -> BoolElt
   return tau;
 end intrinsic;
 
+intrinsic '/'(omega::GrpCharUnitTotElt, omegap::GrpCharUnitTotElt) -> BoolElt
+  {Quotient of two unit characters.}
+
+  require BaseField(omega) eq BaseField(omegap) : "Must have same base field.";
+
+  tau := New(GrpCharUnitTotElt);
+  tau`BaseField := BaseField(omega); // just checked they were the same
+  tau`vals := [omega`vals[i]/omegap`vals[i] : i in [1..#omega`vals]];
+  return tau;
+end intrinsic;
+
+intrinsic '^'(omega::GrpCharUnitTotElt, n::RngIntElt) -> BoolElt
+  {Power of a unit character.}
+
+  tau := New(GrpCharUnitTotElt);
+  tau`BaseField := BaseField(omega); 
+  tau`vals := [omega`vals[i]^n : i in [1..#omega`vals]];
+  return tau;
+end intrinsic;
+
 intrinsic UnitCharacter(F::FldAlg, vals::SeqEnum[RngElt]) -> GrpCharUnitTotElt
   {Create the unit character on the totally positive unit group 
    of the ring of integers of F with values on generators specified by vals.}
