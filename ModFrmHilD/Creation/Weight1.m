63df64acfebed60a557d74572243a457040dde84
Aleksander Horawa <ahorawa@umich.edu>
2022-02-24 22:46:23 -0500

    fixed normalization issue, changed to vprint

--- ModFrmHilD/Creation/Weight1.m
+++ ModFrmHilD/Creation/Weight1.m
@@ -18,15 +18,18 @@ intrinsic Weight1CuspBasis(
   assert IsCompatibleWeight(chi, k);
 
   //Load space of Cusp forms of weight [2,2] and level N.
-  printf "Computing basis of cusp forms of weight [2,2] and level \n %o\n", N;
+  vprintf HilbertModularForms: "Computing basis of cusp forms of weight [2,2] and level \n %o\n", N;
   M2 := HMFSpace(M, N, [2,2]);
   B2 := CuspFormBasis(M2);
   printf "Size of basis is %o.\n", #B2;
 
-  //Load the Eisenstein series of weight [1,1], character chi.
-  printf "Computing Eisenstein series of weight [1, 1] and character %o\n", chiinv;
+  //Load an Eisenstein series of weight [1,1], character chi.
+  vprintf HilbertModularForms: "Computing an Eisenstein series of weight [1, 1] and character %o\n", chiinv;
   M1 := HMFSpace(M, N, [1,1], chiinv);
-  E1 := EisensteinBasis(M1)[1];
+  AdmChars := EisensteinAdmissibleCharacterPairs(M1);
+  require #AdmChars gt 0: "No Eisenstein series of correct character";
+  pair := AdmChars[1];
+  E1 := EisensteinSeries(M1, pair[1], pair[2]);
 
   //Choosing the prime pp.
   if pp cmpeq false then
@@ -41,10 +44,10 @@ intrinsic Weight1CuspBasis(
     break;
     end for;
   end if;
-  printf "Using Hecke operator at prime \n %o \n of norm %o.\n", pp, Norm(pp);
+  vprintf HilbertModularForms: "Using Hecke operator at prime \n %o \n of norm %o.\n", pp, Norm(pp);
 
   //Now we start the big computation.
-  printf "Starting the big computation...\n";
+  vprintf HilbertModularForms: "Starting the big computation...\n";
   cont := true;
 
   V := [];
@@ -56,7 +59,7 @@ intrinsic Weight1CuspBasis(
   dimprev := #V;
 
   while cont do
-    printf "Current dim = %o\n", dimprev;
+    vprintf HilbertModularForms:  "Current dim = %o\n", dimprev;
     TpVprev := [];
     for g in Vprev do
       Append(~TpVprev, HeckeOperator(g, pp));
@@ -64,7 +67,7 @@ intrinsic Weight1CuspBasis(
 
     lindep := LinearDependence(Vprev cat TpVprev);
     dimnew := #lindep;
-    printf "New dim = %o\n", dimnew;
+    vprintf HilbertModularForms: "New dim = %o\n", dimnew;
 
     if dimprev eq dimnew then
       cont := false;
@@ -73,7 +76,7 @@ intrinsic Weight1CuspBasis(
     Vnew := [];
     for vec in lindep do
       f := &+[vec[i]*Vprev[i] : i in [1 .. #Vprev]];
-      Append(~Vnew, f/Coefficient(f, 1*ZF));
+      Append(~Vnew, f);
     end for;
 
     assert #Vnew eq dimnew;
