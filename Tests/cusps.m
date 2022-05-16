c340f3b78c65c22b4974b475755e9c740aea3f88
Sam Schiavone <sam.schiavone@gmail.com>
2022-05-16 15:14:51 -0400

    smaller prime for test

--- Tests/cusps.m
+++ Tests/cusps.m
@@ -13,7 +13,7 @@ end for;
 K<sqrt5> := QuadraticField(5);
 PP := PrimeIdealsOverPrime(K, 31)[1];
 QQ := PrimeIdealsOverPrime(K, 5)[1];
-NN := PP^3*QQ;
+NN := PP*QQ;
 CuspSanityCheck(n : GammaType := "Gamma0");
 for type in GammaTypes do
   assert CuspSanityCheck(NN : GammaType := type);
