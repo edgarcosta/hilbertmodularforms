390d85aaf484ded5ed955819d5e8506d6c01739e
Sam Schiavone <sam.schiavone@gmail.com>
2022-10-24 15:15:17 -0400

    working on Gamma cusp example

--- examples/Gamma-cusp-example.m
+++ examples/Gamma-cusp-example.m
@@ -1,4 +1,5 @@
 F<nu> := QuadraticField(5);
+ZF := Integers(F);
 CuspSanityCheck(2*ZF : GammaType := "Gamma");
 quads := CuspQuadruples(2*ZF, 1*ZF : GammaType := "Gamma");
 for quad in quads do
@@ -10,7 +11,7 @@ Q;
 #Q;
 UQ, mpUQ := UnitGroup(Q);
 U, mpU := UnitGroup(ZF);
-Ugens := [mpU(el) : el in Generators(U)];
+//Ugens := [mpU(el) : el in Generators(U)];
 /*
   cart := CartesianPower(Q,2);
   cart;
@@ -22,8 +23,27 @@ Ugens := [mpU(el) : el in Generators(U)];
   [units[2]*cart0[1][1], units[2]*cart0[1][2]];
 */
 M := Module(ZF,1);
-M;
-quo< M | 2*M >;
-QM := $1;
-DirectSum(QM, QM);
-D, i1, i2, p1, p2 := DirectSum(Q,Q);
+//mpM := Coercion(ZF, M);
+mpM := hom< ZF -> M | x :-> x*M.1>;
+QM, mpQM := quo< M | 2*M >; // I don't think this is what I want...
+mpQtoQM := hom< Q -> QM | x :-> mpQM(mpM(x @@ mpQ))>;
+D, i1, i2, p1, p2 := DirectSum(QM,QM);
+//units := [mpQM(mpM(mpU(el))) : el in Generators(U)];
+//D_seq := [(a @@ mpQ)*D.1+(b @@ mpQ)*D.2 : a,b in Q];
+D_seq := [i1(mpQtoQM(a)) + i2(mpQtoQM(b)) : a,b in Q];
+Remove(~D_seq,1);
+orbits := [];
+while #D_seq ne 0 do
+  d := D_seq[#D_seq];
+  printf "d = %o, in position %o\n", d, #D_seq;
+  //Remove(~D_seq,#D_seq);
+  orb_d := [d];
+  for u in UQ do
+    printf "u = %o\n", u;
+    new := (mpUQ(u) @@ mpQ)*d;
+    printf "new = %o, in position %o\n", new, Index(D_seq,new);
+    Append(~orb_d, new);
+    Remove(~D_seq, Index(D_seq,new));
+  end for;
+  Append(~orbits, orb_d);
+end while;
