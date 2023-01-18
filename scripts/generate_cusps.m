cd3fae5d371a81cac53264385889901f2ab263a6
Edgar Costa <edgarc@mit.edu>
2023-01-18 16:28:07 -0500

    report error

--- scripts/generate_cusps.m
+++ scripts/generate_cusps.m
@@ -36,23 +36,23 @@ assert GammaType in ["Gamma", "Gamma0", "Gamma1"];
 
 
 
-try
-  F := NumberField(MinimalPolynomial(Integers(QuadraticField(D)).2));
-  _, mp := NarrowClassGroup(F);
-  narrow_reps := IdealRepsMapDeterministic(F, mp);
-  ideals := IdealsUpTo(MaxLevelNorm, F);
-  labels := [[StringToInteger(c) : c in Split(LMFDBLabel(elt), ".")] : elt in ideals];
-  ParallelSort(~labels, ~ideals);
-  for NN in ideals do
-    for bb in narrow_reps do
+F := NumberField(MinimalPolynomial(Integers(QuadraticField(D)).2));
+_, mp := NarrowClassGroup(F);
+narrow_reps := IdealRepsMapDeterministic(F, mp);
+ideals := IdealsUpTo(MaxLevelNorm, F);
+labels := [[StringToInteger(c) : c in Split(LMFDBLabel(elt), ".")] : elt in ideals];
+ParallelSort(~labels, ~ideals);
+for NN in ideals do
+  for bb in narrow_reps do
+    try
       G := CongruenceSubgroup(AmbientType, GammaType, F, NN, bb);
       for c in CuspsWithResolution(G) do
         print WriteCuspDataToRow(G, c);
       end for;
-    end for;
+    catch e
+      WriteStderr(Sprtinf("Failed for D = %o, NN =%o , bb=%o", D, LMFDBLabel(NN), LMFDBLabel(bb)));
+      WriteStderr(e);
+      exit 1;
+    end try;
   end for;
-catch e
-  WriteStderr(e);
-  exit 1;
-end try;
-exit;
+end for;
--- scripts/generate_inv.m
+++ scripts/generate_inv.m
@@ -39,26 +39,27 @@ assert GammaType in ["Gamma", "Gamma0", "Gamma1"];
 
 
 
+F := NumberField(MinimalPolynomial(Integers(QuadraticField(D)).2));
+ZF := Integers(F);
+_, mp := NarrowClassGroup(F);
+narrow_reps := IdealRepsMapDeterministic(F, mp);
+ideals := IdealsUpTo(MaxLevelNorm, F);
+labels := [[StringToInteger(c) : c in Split(LMFDBLabel(elt), ".")] : elt in ideals];
+ParallelSort(~labels, ~ideals);
+for NN in ideals do
+  if GCD(NN, 3*D*ZF) ne 1*ZF then
+    continue;
+  end if;
+  for bb in narrow_reps do
 try
-  F := NumberField(MinimalPolynomial(Integers(QuadraticField(D)).2));
-  ZF := Integers(F);
-  _, mp := NarrowClassGroup(F);
-  narrow_reps := IdealRepsMapDeterministic(F, mp);
-  ideals := IdealsUpTo(MaxLevelNorm, F);
-  labels := [[StringToInteger(c) : c in Split(LMFDBLabel(elt), ".")] : elt in ideals];
-  ParallelSort(~labels, ~ideals);
-  for NN in ideals do
-    if GCD(NN, 3*D*ZF) ne 1*ZF then
-      continue;
-    end if;
-    for bb in narrow_reps do
-      G := CongruenceSubgroup(AmbientType, GammaType, F, NN, bb);
-      print WriteGeometricInvariantsToRow(G);
-    end for;
-  end for;
+    G := CongruenceSubgroup(AmbientType, GammaType, F, NN, bb);
+    print WriteGeometricInvariantsToRow(G);
 catch e
+  WriteStderr(Sprtinf("Failed for D = %o, NN =%o , bb=%o", D, LMFDBLabel(NN), LMFDBLabel(bb)));
   WriteStderr(e);
   exit 1;
 end try;
+  end for;
+end for;
 exit;
 
