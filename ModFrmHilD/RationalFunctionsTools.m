37673d4c3f25da817979fb2e5a295d8cfcb4bdb2
Grant Molnar <molnar.grant.5772@gmail.com>
2023-01-17 21:09:48 -0500

    I was looking at 12nth coefficients
    John says we should look at 24nth coefficients

--- ModFrmHilD/RationalFunctionsTools.m
+++ ModFrmHilD/RationalFunctionsTools.m
@@ -13,9 +13,6 @@ intrinsic SubseriesRationalFunction(F::FldFunRatUElt : k := 1) -> FldFunRatUElt
   return Parent(F) ! (Parent(F).1^(v div k)*numLk/denLk);
 end intrinsic;
 
-//BUG: We are claiming that 1/(x-1) has coefficient (x + 1 choose 2)
-//I have my coefficients wrong
-
 //Let F(t) be a rational function, and write F(t) = sum alpha(n) t^n
 // If r1 = r, r2, ..., rk are the roots of the denominator of F, there are polynomials f1(x), ..., fk(x) such that for n large we may write alpha(n) = sum fj(n) rj^n
 // Given r = rj, we return fj(n)
@@ -37,11 +34,11 @@ intrinsic CoefficientComponent(F::FldFunRatUElt : r := 1) -> RngUPolElt
 end intrinsic;
 
 //Let F(t) be a rational function, and write F(t) = sum alpha(n) t^n.
-//Suppose that the coefficients of sum alpha(12 n) t^n are of the form alpha(n) = f(n) for f a fixed polynomial and for all for n large.
+//Suppose that the coefficients of sum alpha(24 n) t^n are of the form alpha(n) = f(n) for f a fixed polynomial and for all for n large.
 //We return the degree of f
 //Returns -1 if f = 0
 //This should be the Kodaira dimension, in a particular context, if memory serves
-intrinsic KodairaDimension(F::FldFunRatUElt : k := 12, r:= 1) -> RngIntElt
+intrinsic KodairaDimension(F::FldFunRatUElt : k := 24, r:= 1) -> RngIntElt
   {Given a rational function (and an index k and a scalar r), returns the degree of the polynomial coefficient of r^n in the Taylor series expansion of the series with knth coefficients extracted}
   SubseriesF := SubseriesRationalFunction(F : k := k);
   if SubseriesF eq 0 then
