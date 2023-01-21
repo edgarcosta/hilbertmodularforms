309220a10ce6345af186accb5fbc71b7822dd924
Edgar Costa <edgarc@mit.edu>
2023-01-21 18:17:16 +0000

    a bunch of verbosity plus tests

--- scripts/generate_inv.m
+++ scripts/generate_inv.m
@@ -41,26 +41,37 @@ assert gamma in ["Gamma", "Gamma0", "Gamma1"];
 
 F := NumberField(MinimalPolynomial(Integers(QuadraticField(D)).2));
 ZF := Integers(F);
-_, mp := NarrowClassGroup(F);
+NCl, mp := NarrowClassGroup(F);
 narrow_reps := IdealRepsMapDeterministic(F, mp);
 ideals := IdealsUpTo(MaxLevelNorm, F);
 labels := [[StringToInteger(c) : c in Split(LMFDBLabel(elt), ".")] : elt in ideals];
 ParallelSort(~labels, ~ideals);
 for NN in ideals do
+  chisum := 0;
+  skipping := (GCD(NN, 3*D*ZF) ne 1*ZF) or (gamma eq "Gamma1" and not IsSquarefree(NN));
   for bb in narrow_reps do
     G := CongruenceSubgroup(ambient, gamma, F, NN, bb);
-    if (GCD(NN, 3*D*ZF) ne 1*ZF) or (gamma eq "Gamma1" and not IsSquarefree(NN)) then
+    if skipping then
       print StripWhiteSpace(Join([LMFDBLabel(G),"SKIPPED"],":"));
     else
       try
         print WriteGeometricInvariantsToRow(G);
+        chisum +:= ArithmeticGenus(G);
       catch e
         print StripWhiteSpace(Join([LMFDBLabel(G),"FAILED"],":"));
-        WriteStderr(Sprintf("Failed WriteGeometricInvariantsToRow for %o", LMFDBLabel(G)));
+        WriteStderr(Sprintf("Failed WriteGeometricInvariantsToRow for %o\n", LMFDBLabel(G)));
         WriteStderr(e);
       end try;
     end if;
   end for;
+  if testarithmeticgenus assigned and not skipping and [ambient, gamma] eq ["GL+", "Gamma0"] then
+    M := GradedRingOfHMFs(F, 0);
+    h := HilbertSeriesCusp(M, NN);
+    chi2 := Coefficient(PowerSeriesRing(Rationals())!h,2) + #NCl;
+    if not chisum eq chi2 then
+        WriteStderr(Sprintf("Failed TestArithmeticGenus for %o %o != %o\n", LMFDBLabel(G), chisum, chi2));
+    end if;
+  end if;
 end for;
 exit;
 
