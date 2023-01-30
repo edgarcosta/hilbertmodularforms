4851d03dab87c68f12f27445f9af6f86e59d706e
Edgar Costa <edgarc@mit.edu>
2023-01-30 12:46:07 -0500

    making examples testable

--- examples/paper-examples.m
+++ examples/paper-examples.m
@@ -1,48 +1,21 @@
-// paper examples
-
-// Example 1
-labels := ["2.2.85.1-1.1-1.1-gl-0", "2.2.85.1-1.1-3.1-gl-0"];
-for label in labels do
+printf "Verying examples 1-4 in A database of basic numerical invariants of Hilbert modular surfaces...";
+// Examples 1, 2, 3
+examples := [*
+  // Example 1
+  <"2.2.85.1-1.1-1.1-gl-0", <ArithmeticGenus, K2, getHZExceptionalNum>,  <4,-8,8>>,
+  <"2.2.85.1-1.1-3.1-gl-0", <ArithmeticGenus, K2, getHZExceptionalNum>, <4,0,2>>,
+  // Example 2
+  <"2.2.44.1-1.1-1.1-sl-0", <ArithmeticGenus, K2, getHZExceptionalNum>, <2, -8, 8>>,
+  <"2.2.44.1-1.1-2.1-sl-0", <ArithmeticGenus, K2, getHZExceptionalNum>, <3, -2, 2>>,
+  //Example 3
+  <"2.2.165.1-1.1-1.1-sl-0",<ArithmeticGenus, K2, getHZExceptionalNum>, <4, -20, 20>>,
+  <"2.2.165.1-1.1-1.1-gl-0",<ArithmeticGenus, K2, getHZExceptionalNum>, <3, -10, 20>>,
+  //Example 4
+  <"2.2.17.1-2.1-1.1-sl-0", <ArithmeticGenus, RationalityCriterion>, <1, true>>,
+  <"2.2.17.1-4.1-1.1-sl-0", <ArithmeticGenus, RationalityCriterion>, <1, true>>
+*];
+for ex in examples do
+  label, fns, expected := Explode(ex);
   G := LMFDBCongruenceSubgroup(label);
-  ArithmeticGenus(G);
-  // 4 // 4
-  K2(G);
-  // -8 // 0
-  getHZExceptionalNum(G);
-  // 8 // 2
-end for;
-
-// Example 2
-labels := ["2.2.44.1-1.1-1.1-sl-0", "2.2.44.1-1.1-2.1-sl-0"];
-for label in labels do
-  G := LMFDBCongruenceSubgroup(label);
-  ArithmeticGenus(G);
-  // 2 // 3
-  K2(G);
-  // -8 // -2
-  getHZExceptionalNum(G);
-  // 8 // 2
-end for;
-
-
-// Example 3
-labels := ["2.2.165.1-1.1-1.1-sl-0", "2.2.165.1-1.1-1.1-gl-0"];
-for label in labels do
-  G := LMFDBCongruenceSubgroup(label);
-  ArithmeticGenus(G);
-  // 4 // 3
-  K2(G);
-  // -20 // -10
-  getHZExceptionalNum(G);
-  // 20 // 20
-end for;
-
-// Example 4
-labels := ["2.2.17.1-2.1-1.1-sl-0", "2.2.17.1-4.1-1.1-sl-0"];
-for label in labels do
-  G := LMFDBCongruenceSubgroup(label);
-  ArithmeticGenus(G);
-  // 1 // 1
-  RationalityCriterion(gamma);
-  // true // true
+  assert <f(G) : f in fns> eq expected;
 end for;
