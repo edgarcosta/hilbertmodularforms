8060de3f33c4fa63908a88519c61556518b97a83
Sam Schiavone <sam.schiavone@gmail.com>
2023-05-08 18:13:37 -0400

    more script tweaks

--- Verification/CanonicalRingsScript.m
+++ Verification/CanonicalRingsScript.m
@@ -1,7 +1,7 @@
 /*
   Usage: run the following command in ~/github/hilbertmodularforms, or wherever spec is located
 
-  parallel -j 16 --joblog joblog --eta magma -b D:={} Verification/CanonicalRingsScript.m  ::: {9..50} > output.txt
+  parallel -j 16 --joblog joblog --eta magma -b D:={} Verification/CanonicalRingsScript.m  ::: {9..50} > Verification/CanonicalRingsArray.m
 */
 
 AttachSpec("spec");
@@ -14,7 +14,6 @@ try
       print Sprintf("R<[x]> := %m;", R);
       print "A := Proj(R);";
       print "eqns := DefiningEquations(S);";
-      print "eqns := DefiningEquations(S);";
       print Sprintf("NN := ideal< Integers(F) | %o >;\n", Generators(NN));
       print "C[NN] := Scheme(A,eqns);";
   end procedure;
@@ -22,9 +21,15 @@ try
 
   //for D in [i : i in [9..50] | IsFundamentalDiscriminant(i)] do
     F := NumberField(MinimalPolynomial(Integers(QuadraticField(D)).2));
-    Write("CanonicalRingsArray.m", Sprintf("F := NumberField(MinimalPolynomial(Integers(QuadraticField(%o)).2));", D));
+    printf "F := NumberField(MinimalPolynomial(Integers(QuadraticField(%o)).2));", D;
     for NN in IdealsUpTo(1,F) do
-      S := HilbertModularVariety(F, NN, 10, 20 : Precision := 10);
+      if Discriminant(F) eq 5 then
+        S := HilbertModularVariety(F, NN, 20, 40 : Precision := 10);
+      elif Discriminant(F) eq 8 then
+        S := HilbertModularVariety(F, NN, 14, 28 : Precision := 10);
+      else
+        S := HilbertModularVariety(F, NN, 10, 20 : Precision := 10);
+      end if;
       WriteCanonicalRingToFile(F,NN,S);
     end for;
 //end for;
