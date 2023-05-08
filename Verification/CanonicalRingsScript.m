b55482f7555bae66e5f4a1bf06ccab14a5504474
Sam Schiavone <sam.schiavone@gmail.com>
2023-05-08 17:59:15 -0400

    update script

--- Verification/CanonicalRingsScript.m
+++ Verification/CanonicalRingsScript.m
@@ -1,28 +1,35 @@
 /*
   Usage: run the following command in ~/github/hilbertmodularforms, or wherever config.m is located
 
-  parallel -j 16 --joblog joblog --eta -a Ds.txt magma -b D:={} ~/Verification/CanonicalRingsScript.m  > output.txt
+  parallel -j 16 --joblog joblog --eta magma -b D:={} Verification/CanonicalRingsScript.m  ::: {9..50} > output.txt
 */
 
-load "config.m";
-ChangeDirectory("Verification");
+AttachSpec("spec");
 
-procedure WriteCanonicalRingToFile(F,S)
-    R<[x]> := CoordinateRing(Ambient(S));
-    Write("CanonicalRingsArray.m", Sprintf("R<[x]> := %m;", R));
-    Write("CanonicalRingsArray.m", "A := Proj(R);");
-    Write("CanonicalRingsArray.m", "eqns := DefiningEquations(S);");
-    Write("CanonicalRingsArray.m", "eqns := DefiningEquations(S);");
-    Write("CanonicalRingsArray.m", Sprintf("NN := ideal< Integers(F) | %o >;\n", Generators(NN)));
-    Write("CanonicalRingsArray.m", "C[NN] := Scheme(A,eqns);");
-end function;
+try 
+  D := StringToInteger(D);
+  if not IsFundamentalDiscriminant(D) then exit 0; end if;
+  procedure WriteCanonicalRingToFile(F,NN,S)
+      R<[x]> := CoordinateRing(Ambient(S));
+      print Sprintf("R<[x]> := %m;", R);
+      print "A := Proj(R);";
+      print "eqns := DefiningEquations(S);";
+      print "eqns := DefiningEquations(S);";
+      print Sprintf("NN := ideal< Integers(F) | %o >;\n", Generators(NN));
+      print "C[NN] := Scheme(A,eqns);";
+  end procedure;
 
-//for D in [i : i in [9..50] | IsFundamentalDiscriminant(i)] do
-  F := NumberField(MinimalPolynomial(Integers(QuadraticField(D)).2));
-  Write("CanonicalRingsArray.m", Sprintf("F := NumberField(MinimalPolynomial(Integers(QuadraticField(%o)).2));", D));
-  for NN in IdealsUpTo(1,F) do
-    S := HilbertModularVariety(F, NN, 10, 20 : Precision := 10);
-    WriteCanonicalRingToFile(F,S);
-  end for;
+
+  //for D in [i : i in [9..50] | IsFundamentalDiscriminant(i)] do
+    F := NumberField(MinimalPolynomial(Integers(QuadraticField(D)).2));
+    Write("CanonicalRingsArray.m", Sprintf("F := NumberField(MinimalPolynomial(Integers(QuadraticField(%o)).2));", D));
+    for NN in IdealsUpTo(1,F) do
+      S := HilbertModularVariety(F, NN, 10, 20 : Precision := 10);
+      WriteCanonicalRingToFile(F,NN,S);
+    end for;
 //end for;
-quit;
+  exit 0;
+catch e
+  WriteStderr(e);
+  exit 1;
+end try;
