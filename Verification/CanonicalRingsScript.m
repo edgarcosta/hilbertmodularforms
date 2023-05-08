7c27c2f2a1f70195a4a07a65e9871b56c29710f1
Sam Schiavone <sam.schiavone@gmail.com>
2023-05-08 17:43:09 -0400

    update script

--- Verification/CanonicalRingsScript.m
+++ Verification/CanonicalRingsScript.m
@@ -1,12 +1,13 @@
+/*
+  Usage: run the following command in ~/github/hilbertmodularforms, or wherever config.m is located
+
+  parallel -j 16 --joblog joblog --eta -a Ds.txt magma -b D:={} ~/Verification/CanonicalRingsScript.m  > output.txt
+*/
+
 load "config.m";
 ChangeDirectory("Verification");
 
-//for D in [i : i in [1..50] | IsFundamentalDiscriminant(i)] do
-for D in [13] do
-  F := NumberField(MinimalPolynomial(Integers(QuadraticField(D)).2));
-  Write("CanonicalRingsArray.m", Sprintf("F := NumberField(MinimalPolynomial(Integers(QuadraticField(%o)).2));", D));
-  for NN in IdealsUpTo(1,F) do
-    S := HilbertModularVariety(F, NN, 10, 20 : Precision := 10);
+procedure WriteCanonicalRingToFile(F,S)
     R<[x]> := CoordinateRing(Ambient(S));
     Write("CanonicalRingsArray.m", Sprintf("R<[x]> := %m;", R));
     Write("CanonicalRingsArray.m", "A := Proj(R);");
@@ -14,5 +15,14 @@ for D in [13] do
     Write("CanonicalRingsArray.m", "eqns := DefiningEquations(S);");
     Write("CanonicalRingsArray.m", Sprintf("NN := ideal< Integers(F) | %o >;\n", Generators(NN)));
     Write("CanonicalRingsArray.m", "C[NN] := Scheme(A,eqns);");
+end function;
+
+//for D in [i : i in [9..50] | IsFundamentalDiscriminant(i)] do
+  F := NumberField(MinimalPolynomial(Integers(QuadraticField(D)).2));
+  Write("CanonicalRingsArray.m", Sprintf("F := NumberField(MinimalPolynomial(Integers(QuadraticField(%o)).2));", D));
+  for NN in IdealsUpTo(1,F) do
+    S := HilbertModularVariety(F, NN, 10, 20 : Precision := 10);
+    WriteCanonicalRingToFile(F,S);
   end for;
-end for;
+//end for;
+quit;
