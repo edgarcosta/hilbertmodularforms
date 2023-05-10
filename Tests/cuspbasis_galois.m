39212f4974d7f7eea29b98b4ae706fc7b7e2038e
Edgar Costa <edgarc@mit.edu>
2023-05-10 09:40:35 -0400

    remove verbose

--- Tests/cuspbasis_galois.m
+++ Tests/cuspbasis_galois.m
@@ -8,7 +8,6 @@ prec:=1;
 M:=GradedRingOfHMFs(F, prec);
 ZF:=Integers(F);
 N:= 23*ZF;
-SetVerbose("HilbertModularForms", 3);
 M2 := HMFSpace(M, N, [2,2]);
 C2 := CuspFormBasis(M2);
 delete M2`CuspFormBasis;
