01d0f565b5e1cb213a05c21e5394a32b7933a211
Edgar Costa <edgarc@mit.edu>
2023-05-10 09:39:57 -0400

    test

---
+++ Tests/cuspbasis_galois.m
@@ -0,0 +1,16 @@
+printf "Testing GaloisDescent argument...";
+F:=QuadraticField(10);
+prec:=1;
+M:=GradedRingOfHMFs(F, prec);
+NumberOfCoefficients(M);
+F:=QuadraticField(10);
+prec:=1;
+M:=GradedRingOfHMFs(F, prec);
+ZF:=Integers(F);
+N:= 23*ZF;
+SetVerbose("HilbertModularForms", 3);
+M2 := HMFSpace(M, N, [2,2]);
+C2 := CuspFormBasis(M2);
+delete M2`CuspFormBasis;
+// recompute without GaloisDescent
+C2 := CuspFormBasis(M2 : GaloisDescent:=false);
