8a0bafb9d72acc000e76a77dc288b0b10aacfff4
Sam Schiavone <sam.schiavone@gmail.com>
2023-05-11 16:18:50 +0000

    moved CanonicalRingsArray.m to hilbertmodularsurfacesdata

--- Verification/CanonicalRingsArray.m
+++
@@ -1,9 +0,0 @@
-/*
-==================================================
-        Canonical Rings Array:
-
-an array C whose entries C[NN] are the Hilbert modular surface with level NN
-==================================================
-*/
-
-C := AssociativeArray();
