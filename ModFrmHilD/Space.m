0e6a58243af6b633fbd9ff374c913a36d5962df3
Eran Assaf <assaferan@gmail.com>
2023-05-12 17:05:35 -0400

    gave up on the trace

--- ModFrmHilD/Space.m
+++ ModFrmHilD/Space.m
@@ -376,7 +376,9 @@ intrinsic CuspDimension(Mk::ModFrmHilD : version:="builtin") -> RngIntElt
       // Edgar: Ben, should one use Strace?
       Mk`CuspDimension := Integers()!Trace(Mk,1*ZF);
       if SequenceToSet(k) eq Set([2]) then
-        Mk`CuspDimension -:= NarrowClassNumber(M) div ClassNumber(ZF);
+	  // !!! TODO : This doesn't work. The fix below also doesn't
+	  Mk`CuspDimension -:= NarrowClassNumber(M);
+          // Mk`CuspDimension -:= NarrowClassNumber(M) div ClassNumber(ZF);
       end if;
     end if;
   end if;
