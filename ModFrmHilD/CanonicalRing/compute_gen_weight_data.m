4341b3cb1a199fbcfad943462339438718dc6b54
Sam Schiavone <sam.schiavone@gmail.com>
2023-05-12 18:34:32 -0400

    scripts for computing data of weights of gens and relations

---
+++ ModFrmHilD/CanonicalRing/compute_gen_weight_data.m
@@ -0,0 +1,21 @@
+// run in the hilbertmodularforms directory by loading config.m and then 
+// load "ModFrmHilD/CanonicalRing/compute_gen_weight_data.m";
+path_to_data := "../hilbertmodularsurfacesdata/CanonicalRingEquations";
+files := Split(Pipe(Sprintf("ls %o", path_to_data), ""), "\n");
+print files;
+quads := [];
+for f in files do
+  F_lab, NN_lab, _, _ := Explode(Split(f, "-"));
+  F := LMFDBField(F_lab);
+  NN := LMFDBIdeal(F, NN_lab);
+  s := Read(Sprintf("%o/%o", path_to_data, f))*"return R, A, eqns, S;";
+  R, A, eqns, S := eval s;
+  quad := [Discriminant(F), Norm(NN), Max(VariableWeights(R)), Max([Degree(el) : el in eqns])];
+  print quad;
+  Append(~quads, quad);
+  Sort(~quads);
+end for;
+
+for quad in quads do
+  Write("ModFrmHilD/CanonicalRing/gen_weight_data.txt", Sprintf("%o\n", quad));
+end for;
