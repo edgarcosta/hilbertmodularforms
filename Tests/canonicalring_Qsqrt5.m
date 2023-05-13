96e62b8d5434997f029fa691f7eca20ce4e0cf8b
Edgar Costa <edgarc@mit.edu>
2023-05-12 22:13:40 -0400

    fixing test

--- Tests/canonicalring_Qsqrt5.m
+++ Tests/canonicalring_Qsqrt5.m
@@ -5,44 +5,49 @@ ZF := Integers(F);
 N := ideal<ZF|[1]>;
 prec := 10;
 R := GradedRingOfHMFs(F, prec);
-B := AssociativeArray();
 M := AssociativeArray();
+C := AssociativeArray();
+E := AssociativeArray();
 for k:=2 to 10 by 2 do
     M[k] := HMFSpace(R, [k, k]);
-    B[k] := Basis(M[k]);
+    C[k] := CuspFormBasis(M[k]);
+    E[k] := EisensteinBasis(M[k]);
+    assert #E[k] eq 1;
 end for;
 
 
-E2 := B[2][1];
-E4 := B[4][1];
+assert #C[2] eq 0;
+E2 := E[2][1];
+assert #C[4] eq 0;
+E4 := E[4][1];
 
 // E2 generate everything at weight 4
 
 assert E2^2 eq E4;
 
 // E2, E6 generate everything at weight 6
-assert #B[6] eq 2;
-E6 := B[6][1];
-cusp_forms6 := B[6][2];
+assert #C[6] eq 1;
+E6 := E[6][1];
+cusp_forms6 := C[6][1];
 f := E6 - E2^3;
 f_normalized := (1/Coefficient(f, 1*ZF))*f;
 assert f_normalized eq cusp_forms6;
 
 // E2, E6 generate everything at weight 8
-assert #B[8] eq 2;
-E8 := B[8][1];
+assert #C[8] eq 1;
+E8 := E[8][1];
 
 assert 536 * E2 * E6 -  175 * E2^4  eq  361 * E8;
 
-cusp_forms8 := B[8][2];
+cusp_forms8 := C[8][1];
 
 g := E8- E2^4;
 g_normalized := (1/Coefficient(g, 1*ZF))*g;
 assert g_normalized eq cusp_forms8;
 
 // E2, E6, E10 generate everything at weight 8
-assert #B[10] eq 3;
-E10 := B[10][1];
+assert #C[10] eq 2;
+E10 := E[10][1];
 
 mon := MonomialsOfWeightedDegree([[2,6,10]],[10]);
 Es := [* E2, E6, E10 *];
@@ -51,7 +56,7 @@ Epowers := [ Product([* Es[i]^(Integers() ! m[i]) : i in [1..#Es] *]) : m in mon
 //E10 is linear independent from the rest
 assert Rank(CoefficientsMatrix(Epowers)) eq #Epowers;
 //the cusp forms can be written with E2, E6 and E10
-assert Rank(CoefficientsMatrix(Epowers cat B[10])) eq #Epowers;
+assert Rank(CoefficientsMatrix(Epowers cat C[10])) eq #Epowers;
 
 
 E20 := EisensteinBasis(HMFSpace(R, [k, k]) where k:=20);
