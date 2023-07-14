374cad37f882b48c8df149e14fe6d1cfe0b312b7
ababei <32378497+ababei@users.noreply.github.com>
2023-07-14 16:36:14 -0400

    Create swap_Qsqrt17_lvl3.m
    test for swap map

---
+++ Tests/swap_Qsqrt17_lvl3.m
@@ -0,0 +1,17 @@
+printf "Testing that the space M_2(Gamma0(5)) for F = Q(sqrt(17)) has nonsymmetric forms"; //use printf with no \n
+// Create the graded ring
+F:=QuadraticField(17);
+prec:=6;
+M:=GradedRingOfHMFs(F, prec);
+
+// Create the space of parallel weight [2,2] and level 7
+ZF:=Integers(F);
+N:= 5*ZF;
+M2 := HMFSpace(M, N, [2,2]);
+
+// Compute the basis //
+B2 := Basis(M2);
+B2sym:=SymmetricBasis(M2);
+
+assert #B2 - #B2sym eq 1;
+
