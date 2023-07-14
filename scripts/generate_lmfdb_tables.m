c429285e351530cc92c7759dda3c702ca0231ce9
Edgar Costa <edgarc@mit.edu>
2023-07-14 13:57:09 +0000

    export scripts

--- scripts/generate_lmfdb_tables.m
+++ scripts/generate_lmfdb_tables.m
@@ -1,8 +1,8 @@
 /*
 # about 1min
-time parallel magma -b D:={} scripts/generate_box.m  ::: {0..3000} > data/group_labels.txt
-for table in invs elliptic; do
-echo ${table}; time parallel -a data/group_labels.txt --joblog data/joblog/lmfdb_${inv}_table.log -j 200 --eta magma -b table:=${invs} label:={} scripts/generate_lmfdb_invariants_table.m  > data/lmfdb_${invs}_table.txt; done
+time parallel magma -b D:={} scripts/generate_box.m  ::: {0..10000} > data/group_labels.txt
+for table in invs elliptic cusps; do
+echo ${table}; time parallel -a data/group_labels.txt --joblog data/joblog/lmfdb_${table}_table.log -j 200 --eta magma -b -S 0 table:=${table} label:={} scripts/generate_lmfdb_tables.m  > data/lmfdb_${table}_table.txt; done
 */
 
 
@@ -18,7 +18,7 @@ handlers := [elt : elt in
   [*
   <"invs", WriteInvariantsHeader, WriteInvariantsRow>,
   <"elliptic", WriteElllipticPointsHeader, WriteElllipticPointsRows>,
-  <"cusps", WriteElllipticPointsHeader, WriteElllipticPointsRows>
+  <"cusps", WriteCuspsHeader, WriteCuspsRows>
   *]
   | table eq elt[1]][1];
 if assigned label then
@@ -28,7 +28,8 @@ if assigned label then
   else
     G := LMFDBCongruenceSubgroup(label);
     try
-      print handlers[3](label);
+      out := handlers[3](label);
+      if #out gt 0 then print out; end if;
       exit 0;
     catch e
       WriteStderr(Sprintf("Failed %o for %o\n", GetIntrinsicName(handlers[3]), label));
