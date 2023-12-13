03307392cd27c4a37f0d019623b3eb244e0a0009
Edgar Costa <edgarc@mit.edu>
2023-12-13 16:14:55 -0500

    disable TraceForm by default for now

--- ModFrmHilD/Basis.m
+++ ModFrmHilD/Basis.m
@@ -44,7 +44,7 @@ intrinsic CuspFormBasis(
   IdealClassesSupport:=false,
   Symmetric:=false,
   GaloisDescent:=true,
-  ViaTraceForm:=true,
+  ViaTraceForm:=false,
   SaveAndLoad:=false
   ) -> SeqEnum[ModFrmHilDElt]
   {returns a basis for cuspspace of M of weight k}
@@ -318,7 +318,7 @@ intrinsic Basis(
   :
   IdealClassesSupport:=false,
   Symmetric:=false,
-  ViaTraceForm:=true
+  ViaTraceForm:=false
   ) -> SeqEnum[ModFrmHilDElt]
   { returns a Basis for the space }
   if not assigned Mk`Basis then
