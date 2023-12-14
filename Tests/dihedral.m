f19de935b0068f57cd53e7a1c7256bceb9e4bc84
Aleksander Horawa <ahorawa@umich.edu>
2023-12-14 16:44:14 -0500

    Fixing dihedral code and the test

--- Tests/dihedral.m
+++ Tests/dihedral.m
@@ -3,9 +3,11 @@ for d in [5, 13] do
 
     for P in PrimesUpTo(500, F) do
         for K in QuadraticExtensionsWithConductor(P, [1,2]) do
-            HF := HeckeCharacterGroup(P);
-            chi := HF ! 1;
-            AH := #PossibleHeckeCharacters(F, P, K, chi);
+            HF := HeckeCharacterGroup(P, [1,2]);
+            AH := 0;
+            for chi in Elements(HF) do
+              AH := AH + #PossibleHeckeCharactersOfK(F, P, K, chi);
+            end for;
             G := ClassGroup(AbsoluteField(K));
             AM := (#G - #quo<G | 2*G>)/2; 
             assert AH eq AM;
