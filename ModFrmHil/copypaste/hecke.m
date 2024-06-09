e396ea748645130c734f9c0b59f45ebb15a420f5
Edgar Costa <edgarc@mit.edu>
2024-06-09 13:40:39 -0400

    fix import

--- ModFrmHil/copypaste/hecke.m
+++ ModFrmHil/copypaste/hecke.m
@@ -12,7 +12,7 @@ freeze;
 
 import "hackobj.m" : Ambient,
                      TopAmbient,
-                     HMF0,
+                     // HMF0, // removed for the hack
                      IsBianchi,
                      BMF_with_ambient,
                      set_quaternion_order;
@@ -72,7 +72,7 @@ end function;
 // M`hecke_matrix_field is not always assigned; when it is not,
 // HeckeOperator returns matrices over the weight_base_field.
 
-/*
+/* hack: replaced via import
 function hecke_matrix_field(M)
   if assigned M`hecke_matrix_field then
     return M`hecke_matrix_field;
@@ -85,7 +85,7 @@ end function;
 */
 
 // The natural field over which Hecke operators can be expressed.
-/*
+/* hack: replaced via import
 function minimal_hecke_matrix_field(M)
   bool, minimal := HasAttribute(M, "hecke_matrix_field_is_minimal");
   if bool and minimal then
@@ -336,7 +336,7 @@ function checks(M, _p, op)
   return true, _, p;
 end function;
 
-/*
+/*  hack: replaced via import
 function operator(M, p, op)
   assert op in {"Hecke", "AL", "DegDown1", "DegDownp"};
 
