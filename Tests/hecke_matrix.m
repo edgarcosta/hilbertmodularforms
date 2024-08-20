f252bec5acab753ab6e503c28437f786fd858e13
Abhijit S Mudigonda <abhijit.mudigonda@gmail.com>
2024-08-20 16:59:26 -0500

    made some tests in hecke_matrix optional, fixed Atkin-Lehner test at primes dividing D

--- Tests/hecke_matrix.m
+++ Tests/hecke_matrix.m
@@ -7,6 +7,7 @@
 
 MAX_PP_NORM := 50;
 NUM_TRIALS := 2;
+OPTIONAL_TESTS := false;
 
 // the default quaternion algebra is the one ramified at all but one infinite place
 // this will thrwo an error if B over a field of even degree.
@@ -16,21 +17,24 @@ function test(F, N : B:=0)
 
   ZF := Integers(F);
 
-  if B eq 0 then
+  if B cmpeq 0 then
     B := QuaternionAlgebra(1*ZF, [InfinitePlaces(F)[i] : i in [2 .. Degree(F)]]);
   end if;
 
   assert BaseField(B) eq F;
   O := MaximalOrder(B);
   D := Discriminant(B);
+  // the level and discriminant are assumed to be coprime
+  assert IsCoprime(N, D);
+
   Gamma := FuchsianGroup(O);
 
   // Hecke matrix at infinity
   assert HeckeMatrix(Gamma, N, "Infinity") eq HeckeMatrix2(Gamma, N, "Infinity");
   d := Nrows(HeckeMatrix(Gamma, N, "Infinity"));
 
-  coprime_pps := [pp : pp in PrimesUpTo(MAX_PP_NORM, F) | GCD(pp, N) eq 1*ZF];
-  level_pps := [fac[1] : fac in Factorization(N) | GCD(fac[1], D) eq 1*ZF];
+  coprime_pps := [pp : pp in PrimesUpTo(MAX_PP_NORM, F) | GCD(pp, N * D) eq 1*ZF];
+  level_pps := [fac[1] : fac in Factorization(N)];
   disc_pps := [fac[1] : fac in Factorization(Discriminant(B))];
 
   for _ in [1 .. NUM_TRIALS] do
@@ -62,30 +66,33 @@ ZF := Integers(F);
 // the dimension of H1 is 2, see Greenberg-Voight
 assert test(F, 4*ZF) eq 2;
 
-pps := PrimesUpTo(15, F);
 // h+(F) = 1, D = 1, N = (7, a)^2 * (2)
+pps := PrimesUpTo(15, F);
 _ := test(F, pps[1]^2 * pps[2]);
 
 // h+(F) = 1, D = (2) * (13, alpha), N = (7, a) * (2)
-B := QuaternionAlgebra(26*ZF, [InfinitePlaces(F)[i] : i in [2 .. Degree(F)]]);
-_ := test(F, 2 * Factorization(7*ZF)[1][1]);
-
-// h+(F) = 1, D = 1, N = 1
-F := NumberField(x^3-x^2-10*x+8);
-ZF := Integers(F);
-// the dimension of H1 is 4, see Greenberg-Voight
-assert test(F, 1*ZF) eq 4;
-
-// h+(F) = 1, D = 1, N = 3
-_ := test(F, 3*ZF);
-
-// h+(F) = 2, D = 1
-F := NumberField(x^3-x^2-9*x+10);
-ZF := Integers(F);
-_ := test(F, 2*ZF);
+B := QuaternionAlgebra(2 * Factorization(13*ZF)[1][1], [InfinitePlaces(F)[i] : i in [2 .. Degree(F)]]);
+_ := test(F, Factorization(7*ZF)[1][1] : B:=B);
 
 // h+(F) = 3, D = 1
 F := NumberField(x^3-x^2-9*x+8);
 ZF := Integers(F);
 pps := PrimesUpTo(15, F);
 _ := test(F, pps[1] * pps[2]);
+
+if OPTIONAL_TESTS then
+  // h+(F) = 1, D = 1, N = 1
+  F := NumberField(x^3-x^2-10*x+8);
+  ZF := Integers(F);
+  // the dimension of H1 is 4, see Greenberg-Voight
+  assert test(F, 1*ZF) eq 4;
+
+  // h+(F) = 1, D = 1, N = 3
+  _ := test(F, 3*ZF);
+
+  // h+(F) = 2, D = 1
+  F := NumberField(x^3-x^2-9*x+10);
+  ZF := Integers(F);
+  _ := test(F, 2*ZF);
+end if;
+
