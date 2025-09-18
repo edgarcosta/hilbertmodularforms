ae195a8c9eeb4c9410140828c4cd06fe9a18965c
Abhijit S Mudigonda <abhijit.mudigonda@gmail.com>
2025-09-18 17:47:51 -0400

    fix largest root of unity

--- ModFrmHilD/FldExt.m
+++ ModFrmHilD/FldExt.m
@@ -893,26 +893,16 @@ intrinsic MinkowskiConstant(F::FldAlg) -> FldReElt
   return Sqrt(Abs(D)) * (4 / pi)^s * n^n / Factorial(n);
 end intrinsic;
 
-intrinsic LargestRootOfUnity(K::FldAlg) -> RngIntElt
+intrinsic LargestRootOfUnity(K::FldAlg) -> RngIntElt, FldElt
   {
     Given a number field K, returns the largest d such that 
     zeta_d lies in K.
   }
-  n := Degree(K);
-
-  R<x> := PolynomialRing(K);
-  p_pows := [];
-  for p in PrimesUpTo(n+1) do
-    k := Floor(Log(p, n+1));
-    root_ct := #Roots(x^(p^k)-1);
-    e := Integers()!Log(p, root_ct);
-    Append(~p_pows, <p, e>);
-  end for;
-
-  m := &*[tup[1]^tup[2] : tup in p_pows];
-  require IsSubfield(CyclotomicField(m), K) : "Something's wrong, the field\
-      Q(zeta_m) isn't a subfield of K";
-  return m;
+  zeta_d := UnitsGenerators(K : exclude_torsion:=false)[1];
+  b, d := IsRootOfUnity(zeta_d);
+  // the first generator should always be torsion
+  assert b;
+  return d, zeta_d;
 end intrinsic;
 
 intrinsic IsGalois(F::FldAlg) -> BoolElt
